8264e3cf845d4ec435b7a737b7764e00eec0e1f6
Vishwahari R <140313931+Vishwahari@users.noreply.github.com>
2026-02-04 22:06:39 +0530

    Time: 1916 ms (5.01%) | Memory: 46.3 MB (83.21%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -6,18 +6,18 @@ class Solution {
             int count = 0;
             int sum = 0;
 
-            for (int i = 1; i * i <= n; i++) {
+            for (int i = 1; i  <= n; i++) {
                 if (n % i == 0) {
                     int d1 = i;
-                    int d2 = n / i;
+                    // int d2 = n / i;
 
                     count++;
                     sum += d1;
 
-                    if (d1 != d2) {
-                        count++;
-                        sum += d2;
-                    }
+                    // if (d1 != d2) {
+                    //     count++;
+                    //     sum += d2;
+                    // }
 
                     if (count > 4) break;
                 }
